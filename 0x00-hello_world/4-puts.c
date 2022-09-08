1173ebd8c30191804a0714f5217e5820959ab2b3
ab4d1 <eng.a.m.badi@gmail.com>
2022-09-08 14:16:34 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,11 @@
 
 int main(void)
 {	
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	/* 
+	 * This prints some text using puts
+	 */
+
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
