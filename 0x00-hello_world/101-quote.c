5433d135a91e6601f6e509480c074a2ffff66952
ab4d1 <eng.a.m.badi@gmail.com>
2022-09-08 16:49:35 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,11 +7,21 @@
  *
  * Return: Always(0)
  *
+ * makeItSo - Print string
+ * 	str - string
+ * Return: no return
  */
+
+void makeItSo(char *str)
+{
+	if (*str == '\0')
+		return;
+	makeItSo(str + 1);
+	putchar(*str);
+}
 
 int main(void)
 {
-	fputs("and that piece of art is useful", stdout);
-	fputs("\" - Dora Korpar, 2015-10-19\n", stdout);
+	makeItSo("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (0);
 }
