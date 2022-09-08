b9f4b19a40a1ef9681d84c66dbab4054a1613556
ab4d1 <eng.a.m.badi@gmail.com>
2022-09-08 16:00:18 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@
 
 int main(void)
 {
-	char *string = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char *string = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	fputs(string, stdout);
 	return (0);
