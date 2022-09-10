5057bfae9449a68528f757bcf7b06ea6e5449a23
ab4d1 <eng.a.m.badi@gmail.com>
2022-09-10 12:51:27 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * print alphabet
+ * Return:0
+ */
+
+int main(void)
+{
+
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int u = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = i + 1; k < 10; k++)
+			{
+				for (u = j + 1; u < 10; u++)
+				{
+
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar(' ');
+					putchar('0' + k);
+					putchar('0' + u);
+
+					if (!(i == 7 && j == 8 && k == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+
+	return (0);
+}
