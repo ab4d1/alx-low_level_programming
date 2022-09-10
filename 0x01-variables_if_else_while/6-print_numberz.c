5965105fed5a84bd84db5d3ce65247698df8d8fb
ab4d1 <eng.a.m.badi@gmail.com>
2022-09-10 11:54:27 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * print alphabet
+ * Return:0
+ */
+
+int main(void)
+{
+
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
+
+	putchar('\n');
+
+
+	return (0);
+}
