1827a3ee5b7d01e7a24ccf7bd73ca14be40cf5f1
ab4d1 <eng.a.m.badi@gmail.com>
2022-09-10 11:48:20 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * print alphabet
+ * Return:0
+ */
+
+int main(void)
+{
+
+	char alpha = 'a';
+	int i = 0;
+
+	for (i = 0; i < 26; i++)
+	{
+		if (!(i == 4 || i == 14))
+			putchar(alpha + i);
+	}
+
+	putchar('\n');
+
+
+	return (0);
+}
