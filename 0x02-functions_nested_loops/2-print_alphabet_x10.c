f43e4c5439c4aa06dd5101264426f56fe2670d21
ab4d1 <eng.a.m.badi@gmail.com>
2022-09-15 02:46:11 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet
+ *
+ * Return:void
+ */
+
+void print_alphabet(void)
+{
+
+	char alpha = 'a';
+	int j = 0;
+	int i = 0;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 26; i++)
+		{
+			_putchar(alpha + i);
+		}
+	}
+	_putchar('\n');
+}
