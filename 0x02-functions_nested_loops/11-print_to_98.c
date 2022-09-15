97ef60809bdccfbf5627206232a23f5bae1ffdc6
ab4d1 <111003522+ab4d1@users.noreply.github.com>
2022-09-15 23:42:01 +0200

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,12 +1,30 @@
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * add - add
- *@n: int a
- * Return:int
+ * print_to_98 - add
+ *@n: int n
+ * Return:void
  */
 
 void print_to_98(int n)
 {
-	
+	int i = 0;
+
+	if (n < 98)
+	{
+		for (i = n; i < 98; i++)
+		{
+			printf("%i, ", i);
+		}
+		printf("%i", 98);
+	}
+	if (n > 98)
+	{
+		for (i = n; i > 98; i--)
+		{
+			printf("%i, ", i);
+		}
+		printf("%i", 98);
+	}
 }
