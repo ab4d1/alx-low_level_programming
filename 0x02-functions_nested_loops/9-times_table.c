b1bee13c5cef078188fb029ac2b9a1291d2f208d
ab4d1 <111003522+ab4d1@users.noreply.github.com>
2022-09-15 15:11:11 +0200

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,7 +26,7 @@ void times_table(void)
 				_putchar('0' + m % 10);
 			} else if (m / 10)
 			{
-				_putchar(' ';
+				_putchar(' ');
 				_putchar('0' + (m / 10) % 10);
 				_putchar('0' + m % 10);
 			} else
