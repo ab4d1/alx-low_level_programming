0a89143627e4c802e99405a8262ef782f79f8bd3
ab4d1 <111003522+ab4d1@users.noreply.github.com>
2022-09-15 13:55:48 +0200

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,9 +8,9 @@
 
 int print_last_digit(int n)
 {
-	if (n > 0)
-		_putchar('0' + (n % 10));
-	else
-		_putchar('0' + (-n % 10));
+	if (n < 0)
+		n = -n;
+	_putchar('0' + (n % 10));
+
 	return (n % 10);
 }
