a40b69a4725788ecc6b1c9b42d1f4e1a55f19981
ab4d1 <eng.a.m.badi@gmail.com>
2022-09-15 05:24:02 -0700

    5-sign.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,26 +8,26 @@
 
 void jack_bauer(void)
 {
-    int i = 0;
-    int j = 0;
-    int k = 0;
-    int n = 0;
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int n = 0;
 
-    for (i = 0; i < 3; i++)
-    {
-        _putchar('0' + i);
-        for (j = 0; j < 4; j++)
-        {
-            _putchar('0' + j);
-            _putchar(':');
-            for (k = 0; k < 6; k++)
-            {
-                _putchar('0' + k);
-                for (n = 0; n < 10; n++)
-                {
-                    _putchar('0' + n);
-                }
-            }
-        }
-    }
+	for (i = 0; i < 3; i++)
+	{
+		_putchar('0' + i);
+		for (j = 0; j < 4; j++)
+		{
+			_putchar('0' + j);
+			_putchar(':');
+			for (k = 0; k < 6; k++)
+			{
+				_putchar('0' + k);
+				for (n = 0; n < 10; n++)
+				{
+					_putchar('0' + n);
+				}
+			}
+		}
+	}
 }
