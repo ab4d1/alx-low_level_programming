fc4ed7eee6a51762ba82a43575cd2fba21a8ca2f
ab4d1 <111003522+ab4d1@users.noreply.github.com>
2022-09-16 10:29:24 +0200

    more 10x

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,15 @@
 void more_numbers(void)
 {
 	int i = 0;
+	int j = 0;
 
-	for (i = 0; i < 15; i++)
-	{
-		if (i / 10)
-			_putchar('0' + (i / 10) % 10);
+	for (j = 0; j < 10; j++)
+		for (i = 0; i < 15; i++)
+		{
+			if (i / 10)
+				_putchar('0' + (i / 10) % 10);
 
-		_putchar('0' + i % 10);
-	}
-	_putchar('\n');
+			_putchar('0' + i % 10);
+		}
+		_putchar('\n');
 }
