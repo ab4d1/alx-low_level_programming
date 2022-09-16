251dc07638d83bdd717f62d1dbdc195703cf2617
ab4d1 <111003522+ab4d1@users.noreply.github.com>
2022-09-16 09:58:03 +0200

    print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print_numbers
+ * Return: print
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
