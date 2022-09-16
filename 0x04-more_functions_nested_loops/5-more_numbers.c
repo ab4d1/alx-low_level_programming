d46b0def51e4ca620724c65c25e0fa6b80518240
ab4d1 <111003522+ab4d1@users.noreply.github.com>
2022-09-16 10:20:56 +0200

    most

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - numbs
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	for (i = 0; i < 15; i++)
+	{
+		if (i / 10)
+			_putchar('0' + (i / 10) % 10);
+			_putchar('0' + i % 10);
+		else
+			_putchar('0' + i);
+	}
+	_putchar('\n');
+}
