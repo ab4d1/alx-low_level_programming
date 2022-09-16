32c243152ac3b93fc237526751c2199bff7f0364
ab4d1 <111003522+ab4d1@users.noreply.github.com>
2022-09-16 10:46:26 +0200

    diag

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_diagonal - line
+ *@n: int
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('\\');
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+	}
+	_putchar('\n');
+}
