3f1f6592421603a9d4a6a35ea50b3d892e73a023
ab4d1 <111003522+ab4d1@users.noreply.github.com>
2022-09-17 12:00:21 +0200

    0,

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,9 +20,11 @@ void print_times_table(int n)
 	}
 	else
 	{
-		for (i = 0; i < n; i++)
+		_putchar('0');
+		_putchar(',');
+		for (i = 1; i < n; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 1; j < n; j++)
 			{
 				mul = i * j;
 				for (k = 10000; k > 0; k = k / 10)
