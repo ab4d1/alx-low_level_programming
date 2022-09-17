c731cd44a437b74bf4467dfd6757dead18323e1f
ab4d1 <111003522+ab4d1@users.noreply.github.com>
2022-09-17 23:24:18 +0200

    fibo

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -15,15 +15,13 @@ int main(void)
 	long int b = 2;
 	long int sum = 0;
 
-	printf("%li, %li, ", a, b);
 	for (i = 1; i < 48; i++)
 	{
 		sum = a + b;
+		if ((a % 2 == 0) && (b % 2 == 0))
+			printf("%li", sum);
 		a = b;
 		b = sum;
-		printf("%li, ", sum);
 	}
-	sum = a + b;
-	printf("%li\n", sum);
 	return (0);
 }
