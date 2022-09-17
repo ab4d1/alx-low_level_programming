b0d147ea67b09a0c9ef4a888698ac068099cc537
ab4d1 <111003522+ab4d1@users.noreply.github.com>
2022-09-17 12:19:38 +0200

    ,

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -27,16 +27,8 @@ void print_times_table(int n)
 			for (j = 1; j < n + 1; j++)
 			{
 				mul = i * j;
-				if (mul / 1000)
+				if (mul / 100)
 				{
-					_putchar('0' + (mul / 1000) % 10);
-					_putchar('0' + (mul / 100) % 10);
-					_putchar('0' + (mul / 10) % 10);
-					_putchar('0' + (mul) % 10);
-				}
-				else if (mul / 100)
-				{
-					_putchar(' ');
 					_putchar('0' + (mul / 100) % 10);
 					_putchar('0' + (mul / 10) % 10);
 					_putchar('0' + (mul) % 10);
@@ -44,13 +36,11 @@ void print_times_table(int n)
 				else if (mul / 10)
 				{
 					_putchar(' ');
-					_putchar(' ');
 					_putchar('0' + (mul / 10) % 10);
 					_putchar('0' + (mul) % 10);
 				}
 				else
 				{
-					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar('0' + (mul) % 10);
