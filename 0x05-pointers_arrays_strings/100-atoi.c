4ca4b0599613fe43434bb727c1c5b0e33c2c3bb1
ab4d1 <111003522+ab4d1@users.noreply.github.com>
2022-09-20 14:21:47 +0200

    break

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,6 +11,7 @@ int _atoi(char *s)
 	int i = 0;
 	int num = 0;
 	int sign = -1;
+	int flag = 0;
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
@@ -20,7 +21,9 @@ int _atoi(char *s)
 		{
 			num = num * 10;
 			num = num - (*(s + i) - '0');
-		}
+			flag = 1;
+		} else if (flag == 1)
+			break;
 	}
 
 
