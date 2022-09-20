98000cfa1fd45de88f7152961e17f180d8eb8faf
ab4d1 <111003522+ab4d1@users.noreply.github.com>
2022-09-20 12:28:58 +0200

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - puts2
+ *@str: pointer to int
+ * Return: print
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (i = 0; *(str + i) != '\n'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
