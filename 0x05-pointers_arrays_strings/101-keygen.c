001e5411e22a080571d2ac527d526aafc6dc7c7a
ab4d1 <111003522+ab4d1@users.noreply.github.com>
2022-09-20 23:31:50 +0200

    i dont know

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,20 +1,34 @@
 #include "main.h"
 
 /**
+ * _rand - _rand
+ *@n:int
+ *Return:rand
+ */
+
+unsigned long int _rand(unsigned long int next)
+{
+    next = next * 1103515245 + 12345;
+    next = ((unsigned)(next/65536) % 32768);
+}
+
+/**
  * main - main
  *@s: pointer to int
  * Return: print
  */
 
-int main(void)
+char *main(void)
 {
-	int i = 0;
-	int len = 0;
+	unsigned long int next = 1;
+	char pass[30];
 
-	for (i = 0; i < len; i++)
+	for (i = 0; i < 30; i++)
 	{
-		len++;
+		
+		pass[i] = ' ' + _rand(next) % 100;
+		next++;
 	}
 
-	return (0);
+	return (pass);
 }
