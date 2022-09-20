e087666c8db483a6a3b2f93c66aad4ff934f4858
ab4d1 <111003522+ab4d1@users.noreply.github.com>
2022-09-20 13:51:19 +0200

    &

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,11 +11,13 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 	int len = 0;
+	char tmp[1000];
 
-	for (i = 0; *(s + i) != '\0'; i++)
+	for (i = 0; *(src + i) != '\0'; i++)
 	{
-		len++;
+		tmp[i] = *(src + i);
 	}
-
-	return (len);
+	tmp[i] = *(src + i);
+	*dest = tmp;
+	return (&tmp);
 }
