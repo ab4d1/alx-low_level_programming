a2faaf742dd06748b0decd9c286f1217a3047d09
ab4d1 <111003522+ab4d1@users.noreply.github.com>
2022-09-20 14:06:17 +0200

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _atoi - _atoi
+ *@s: pointer to int
+ * Return: print
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int num = 0;
+	int sign = 0;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) == '+')
+			sign++;
+		else if (*(s + i) == '-')
+			sign--;
+		else if (*(s + i) >= '0' && *(s + i) >= '9')
+	}
+
+	if (sign < 0)
+		num = -num;
+	return (num);
+}
