c193bee25627943697bcf4f399e8d4f3c87a519a
ab4d1 <111003522+ab4d1@users.noreply.github.com>
2022-09-20 11:59:08 +0200

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - print_rev
+ *@s: pointer to int
+ * Return: print
+ */
+
+void print_rev(char *s)
+{
+	int l = _strlen(*s);
+	int i = 0;
+
+	for (i = l-1; i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
