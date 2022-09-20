5388524c6b1eb8bf94fd2b48a1156ce8f565f89a
ab4d1 <111003522+ab4d1@users.noreply.github.com>
2022-09-20 13:43:02 +0200

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - _strcpy
+ *@dest: pointer to int
+ *@src: pointer to int
+ * Return: print
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int len = 0;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		len++;
+	}
+
+	return (len);
+}
