4463a66f5a32e2d8a2d4f0041953c0ff05635554
ab4d1 <111003522+ab4d1@users.noreply.github.com>
2022-09-20 12:19:14 +0200

    j

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,6 +10,7 @@ void rev_string(char *s)
 {
 	char *rev = s;
 	int i = 0;
+	int j = 0;
 	int len = 0;
 
 	for (i = 0; *(s + i) != '\0'; i++)
@@ -19,5 +20,6 @@ void rev_string(char *s)
 	for (i = len - 1; i >= 0; i--)
 	{
 		*(rev + i - len) = *(s + i);
+		j++;
 	}
 }
