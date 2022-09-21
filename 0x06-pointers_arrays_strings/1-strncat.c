9dd1b6b61cec6ad4db0947c211baac4a7c8e98e9
ab4d1 <111003522+ab4d1@users.noreply.github.com>
2022-09-21 16:51:42 +0200

    < n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - _strncat
+ *@dest: pointer to int
+ *@src: pointer to int
+ *@n: int
+ * Return: print
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int len = 0;
+
+	for (i = 0; *(dest + i) != '\0'; i++)
+	{
+		len++;
+	}
+	for (i = 0; *(src + i) != '\0' && i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	*(dest + i) = *(src + i);
+	return (dest);
+}
