ea90d6685c3fed8106994a1f16c54b7f5d366194
ab4d1 <eng.a.m.badi@gmail.com>
2022-09-26 23:43:23 +0200

    something

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -16,9 +16,9 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		for (j = 0; *(accept + i) != '\0'; i++)
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
-			if (*(s + i) == *(accept + i))
+			if (*(s + i) == *(accept + j))
 				sum++;
 		}
 	}
