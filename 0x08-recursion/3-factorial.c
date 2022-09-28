0d175c81e2e3eb9b8ca1bb9f07258ef43a42945c
ab4d1 <eng.a.m.badi@gmail.com>
2022-09-28 10:09:02 +0200

    fact

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * factorial - _strchr
+ *@n: pointer to int
+ * Return: print
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return 1;
+	return (n + factorial(n - 1));
+}
