67776aa3bb201fb2f27df450e0a517bda3a5970f
ab4d1 <eng.a.m.badi@gmail.com>
2022-09-28 20:07:09 +0200

    diag

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagsums - _strchr
+ *@a: pointer to int
+ *@size: pointer to int
+ * Return: print
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i = 0;
+	int j = 0;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		sum1 += a[i][i];
+		sum2 += a[i][size - i - 1]; 
+	}
+	printf("%i, %i", sum1, sum2);
+}
