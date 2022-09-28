7593f2354b9b97badd877c24ebbdb565943c79bc
ab4d1 <eng.a.m.badi@gmail.com>
2022-09-28 10:15:59 +0200

    x*

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - _strchr
+ *@x: pointer to int
+ *@y: pointer to int
+ * Return: print
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 1)
+	{
+		return (x);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
