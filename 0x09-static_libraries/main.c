483569ce83ed8cb0d0e193adc6bc0ef0c9b64d64
ab4d1 <eng.a.m.badi@gmail.com>
2022-09-30 23:44:13 +0200

    README

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+int main(void)
+{
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    int a = _strlen("ssssss");
+    _putchar('0' + a);
+    return (0);
+}
