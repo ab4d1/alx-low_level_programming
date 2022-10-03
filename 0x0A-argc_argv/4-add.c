1ca872907914218eea035591a8202c7e396ea824
ab4d1 <eng.a.m.badi@gmail.com>
2022-10-03 13:56:11 +0200

    space

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -21,10 +21,9 @@ int main(int argc, char **argv)
 		printf("0\n");
 		return (0);
 	}
-
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; *(argv[i] + j) !='\0'; j++)
+		for (j = 0; *(argv[i] + j) != '\0'; j++)
 		{
 			if (*(argv[i] + j) >= '0' && *(argv[i] + j) <= '9')
 			{
