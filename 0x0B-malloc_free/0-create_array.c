7d881a47f818b2917966b9bc13a4774160828b26
ab4d1 <eng.a.m.badi@gmail.com>
2022-10-03 23:48:34 +0200

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	ar = maloc(sizeof(char) * size);
+	ar = malloc(sizeof(char) * size);
 	if (*(ar) == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
