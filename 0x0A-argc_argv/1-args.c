21d388eef9804b7e9b4419ecd97faa503c6c581f
ab4d1 <eng.a.m.badi@gmail.com>
2022-10-03 12:19:33 +0200

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry
+ * docss
+ * @argc: no. of args
+ * Return:0
+ */
+
+int main(int argc)
+{
+	printf("%i", argc - 1);
+	exit(EXIT_SUCCESS);
+}
