ffea2bf9941512f440c8ba1cb10c695e43d87ede
ab4d1 <eng.a.m.badi@gmail.com>
2022-10-03 23:49:36 +0200

    + 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	ar = malloc(sizeof(char) * size);
-	if (*(ar) == NULL)
+	if (*(ar + 0) == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 	{
