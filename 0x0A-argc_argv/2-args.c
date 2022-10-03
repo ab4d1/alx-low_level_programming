906d1ba6ff8034f29302a54e9acdf98d7b0b484c
ab4d1 <eng.a.m.badi@gmail.com>
2022-10-03 12:52:05 +0200

    argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry
+ * docss
+ * @argc: no. of args
+ * @argv: args
+ * Return:0
+ */
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	exit(EXIT_SUCCESS);
+}
