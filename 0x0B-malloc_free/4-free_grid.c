9f0409f84ca3fec11e7ac075a52a6b2497b06469
ab4d1 <eng.a.m.badi@gmail.com>
2022-10-04 14:05:50 +0200

    free

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -14,26 +14,12 @@ void free_grid(int **grid, int height)
 {
 	int **grid;
 	int i;
-	int j;
 
-	if (width <= 0 || height <= 0)
+	if (height <= 0)
 		return (NULL);
-
-	grid = (int **)malloc(sizeof(int *) * width);
-	if (grid == NULL)
-		return (NULL);
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
-		grid[i] = (int *)malloc(sizeof(int) * height);
-		if (grid[i] == NULL)
-			return (NULL);
+		free(grid[i]);
 	}
-	for (i = 0; i < width; i++)
-	{
-		for (j = 0; j < height; j++)
-		{
-			grid[i][j] = 0;
-		}
-	}
-	return (grid);
+	free(grid);
 }
