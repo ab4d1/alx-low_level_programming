85ccd660a6137eae8bbc0ebf80ca5e1b7cedf148
ab4d1 <eng.a.m.badi@gmail.com>
2022-10-04 23:27:03 +0200

    wbn

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,24 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-/**
- * strtow - print alphabet
- *@str:size
- * Return:void
- */
-
-char **strtow(char *str)
+int count_words(char *str)
 {
-	unsigned long int i = 0;
-	int j = 0;
-	int len = 0;
+	unsigned long int i;
 	int words = 0;
-	int word = 0;
 	int flag = 1;
-	char **s;
-
-	if (str[0] == '\0' || str == NULL)
-		return (NULL);
 
 	for (i = 0; i <= strlen(str); i++)
 	{
@@ -32,14 +19,43 @@ char **strtow(char *str)
 		else if (str[i] != ' ')
 			flag = 0;
 	}
+	return (words);
+}
 
+void free_array(char **array, int size)
+{
+	while (size >= 0)
+	{
+		free(array[size]);
+		size--;
+	}
+	free(array);
+}
+
+/**
+ * strtow - print alphabet
+ *@str:size
+ * Return:void
+ */
+
+char **strtow(char *str)
+{
+	unsigned long int i = 0;
+	int j, words;
+	int len = 0;
+	int word = 0;
+	int flag = 1;
+	char **s;
+
+	if (str[0] == '\0' || str == NULL)
+		return (NULL);
+	words = count_words(str);
 	s = malloc((words + 1) * sizeof(char *));
 	if (s == NULL || words == 0)
 	{
 		free(s);
 		return (NULL);
 	}
-	flag = 1;
 	for (i = 0; i <= strlen(str); i++)
 	{
 		if ((str[i] == ' ' || str[i] == '\0') && flag == 0)
@@ -47,31 +63,22 @@ char **strtow(char *str)
 			*(s + word) = malloc((len + 1) * sizeof(char));
 			if (s[word] == NULL)
 			{
-				while (word >= 0)
-				{
-					free(s[word]);
-					word--;
-				}
-				free(s);
+				free_array(s, word);
 				return (NULL);
 			}
 			for (j = 0; j < len; j++)
 			{
 				s[word][j] = (char)str[i - len + j];
-				/*printf("%c, %li", str[i - len + j], i - len + j);*/
 			}
-			/*printf("%s\n", s[word]);*/
 			s[word][j] = '\0';
 			word++;
 			flag = 1;
 			len = 0;
-		}
-		else if (str[i] != ' ')
+		} else if (str[i] != ' ')
 		{
 			len++;
 			flag = 0;
 		}
-		/*printf("c %c\ti= %lu\tword %i\tflag %i\tlen %i\n", str[i], i, word, flag, len);*/
 	}
 	s[word] = NULL;
 	return (s);
