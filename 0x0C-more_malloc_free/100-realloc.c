635e3ba67dcae11f5bb13cd4839f7e02a52c961d
ab4d1 <eng.a.m.badi@gmail.com>
2022-10-06 23:53:32 +0200

    .

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _realloc - print alphabet
+ *@ptr:size
+ *@old_size:size
+ *@new_size:size
+ * Return:void
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *ret;
+
+	ret = malloc(b);
+	if (ret == NULL)
+		exit(98);
+	return (ret);
+}
