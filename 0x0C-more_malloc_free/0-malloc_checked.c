906e72cdc40726712ac75f6f7a7c9f941037c996
ab4d1 <eng.a.m.badi@gmail.com>
2022-10-06 15:05:27 +0200

    test

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - print alphabet
+ *@b:size
+ * Return:void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ret;
+
+	ret = malloc(b);
+	if (ret == NULL)
+		exit(EXIT_FAILURE);
+	return (ret);
+}
