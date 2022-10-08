106a870a7e3cb2bb031fa24403cb44c76e4465f8
ab4d1 <eng.a.m.badi@gmail.com>
2022-10-08 15:59:34 +0200

    new = old

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,8 +16,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	void *temp_ptr;
 	unsigned int min = old_size;
 
-	if (new_size == old_size)
-		return (ptr);
+
 	else if (new_size == 0)
 	{
 		free(ptr);
