a762536a8b1274ee382b5f5879437aa9395ee2fa
ab4d1 <eng.a.m.badi@gmail.com>
2022-10-11 16:46:55 +0200

    docs

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,8 +3,10 @@
 #include "dog.h"
 
 /**
- * print_dog - print alphabet
- *@d:size
+ * new_dog - print alphabet
+ *@name:size
+ *@age:size
+ *@owner:size
  * Return:void
  */
 dog_t *new_dog(char *name, float age, char *owner)
