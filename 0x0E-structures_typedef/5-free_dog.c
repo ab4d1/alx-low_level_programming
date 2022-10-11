32e588e8cac92647ade9648ffd800ea5d5d97333
ab4d1 <eng.a.m.badi@gmail.com>
2022-10-11 17:29:27 +0200

    free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - print alphabet
+ *@d:size
+ * Return:void
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
