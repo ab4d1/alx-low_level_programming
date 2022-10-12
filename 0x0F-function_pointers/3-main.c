77ec6300f7826f1473b2c6a834cc2574af4e805f
ab4d1 <eng.a.m.badi@gmail.com>
2022-10-12 23:08:22 +0200

    NULL

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,12 +15,22 @@ int main(int argc, char **argv)
 {
 	int a = 0;
 	int b = 0;
+	int (*op_func)(int, int);
 
 	if (argc != 4)
-		exit(EXIT_FAILURE);
+	{
+		printf("Error\n");
+		exit(98);
+	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
-	printf("%i\n",(get_op_func(argv[2]))(a,b));
+	op_func = get_op_func(argv[2]);
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%i\n", op_func(a, b));
 	return (0);
 }
