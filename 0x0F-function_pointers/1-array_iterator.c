b4778df91549c1834c5298e6aa516938e62e607a
ab4d1 <eng.a.m.badi@gmail.com>
2022-10-12 20:49:02 +0200

    action

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - print alphabet
+ *@array:char
+ *@size:char
+ *@action:char
+ * Return:void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
