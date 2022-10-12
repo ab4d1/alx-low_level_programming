3bf65ff35462f6cd0b2d7e99d72145cc61a46697
ab4d1 <eng.a.m.badi@gmail.com>
2022-10-12 22:42:36 +0200

    no mallloc

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,19 +15,12 @@ int main(int argc, char **argv)
 {
 	int a = 0;
 	int b = 0;
-	char *s;
 
 	if (argc != 4)
 		exit(EXIT_FAILURE);
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	s = malloc(sizeof(char) * 2);
-	if (s == NULL)
-	{
-		free (s);
-		exit(EXIT_FAILURE);
-	}
-	s = argv[2];
+
 	printf("%i\n",(get_op_func(argv[2]))(a,b));
 	return (0);
 }
