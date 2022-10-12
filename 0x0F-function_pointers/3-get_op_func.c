8837998c44d30e4b3b7eb9d1119ac1591bcd6fe9
ab4d1 <eng.a.m.badi@gmail.com>
2022-10-13 00:21:16 +0200

    ++

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,12 +19,12 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}};
 	int i;
 
-	i = 4;
-	while (i >= 0)
+	i = 0;
+	while (i < 5)
 	{
 		if (s != NULL && (ops[i].op)[0] == s[0])
 			return (ops[i].f);
-		i--;
+		i++;
 	}
 	return (NULL);
 }
