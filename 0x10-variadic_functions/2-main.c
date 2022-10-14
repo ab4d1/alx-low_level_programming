d767b5e18fb19f719e9058a75e89c8012d01c25d
ab4d1 <eng.a.m.badi@gmail.com>
2022-10-14 21:03:24 +0200

    no nil

--- 0x10-variadic_functions/2-main.c
+++ 0x10-variadic_functions/2-main.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "variadic_functions.h"
 
 /**
@@ -8,6 +7,6 @@
  */
 int main(void)
 {
-    print_numbers(NULL, 4, 0, 98, -1024, 402);
+    print_strings(", ", 2, "Jay", "Django");
     return (0);
 }
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - print alphabet
+ *@separator:char
+ *@n:char
+ * Return:void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(args, char *));
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
+	}
+	va_end(args);
+	printf("\n");
+}
